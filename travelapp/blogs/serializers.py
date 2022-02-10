c118648ea31690aa67695375a4ebe4601228e07c
anuragshukla07 <anuragshukla2k12@gmail.com>
2022-02-10 16:44:45 +0530

    'certain_changes'

--- travelapp/blogs/serializers.py
+++ travelapp/blogs/serializers.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from blogs import models
 
-# class TagSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.Tag
-#         fields = '__all__'
-
-
 class ArticleSerializer(serializers.ModelSerializer):
 
     # tags = TagSerializer(many = True, read_only = True)
