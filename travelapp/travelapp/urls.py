7f9dd81056ea3f03665e5c86e5ad58ae7d7d9226
anuragshukla07 <anuragshukla2k12@gmail.com>
2022-02-11 17:30:31 +0530

    'added_register_api'

--- travelapp/travelapp/urls.py
+++ travelapp/travelapp/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/',include('blogs.urls')),
+    path('accounts/',include('accounts.urls')),
     # path('api-auth/', include('rest_framework.urls')),
 ]
